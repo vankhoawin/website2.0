cb0e62af77c1c5175d8835793abba706263f0805
Van <vnguyen94@gmail.com>
2014-06-29 14:26:54 -0700

    fix css, start on page templates

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from pyfb import Pyfb
 
 app = Flask(__name__)      
@@ -12,37 +12,47 @@ def photography():
     FACEBOOK_APP_ID = '511339372322388'
     facebook = Pyfb(FACEBOOK_APP_ID)
 
-    token = 'CAACEdEose0cBAK2js6ZALAE3QltVi6krtLJ3yDDeYgVUp0pr8dKTyc26eGlamH9IYFMAhZC6AXvsncs86kqpa3JKRz1yLMXWhFRX7BZASl1XSYMCfrUJ3HyI3pcocMmCeMlvXIFxEIZCKafbahVNkHOoPZAZCwanw2LLbANkRsm7z7Nopxl7duyBmhZCeqcrBUZD'
+    token = 'CAACEdEose0cBADWohTjQVtZBt6MY1lSaJvJ6sHy7tcgmXrdhZCBI17wQTY5X4ZB83lqPB00z84KTzp3Go6lhL7DnnwtJn95ZAsbP9YrC6exMiMujxPCLiV3CeB2c32R9j9LHcz44lkOjEG3HZAFMDFPEnWBQKq0G3irSkj2K1dWtm7npAfbZByxmnXO7cnHm0ZD'
     facebook.set_access_token(token)
 
     #Gets info about myself
     me = facebook.get_myself()
 
-    print "Name: %s" % me.name
+    #print "Name: %s" % me.name
 
     photos = facebook.get_photos()
 
-    print me.id
+    #print me.id
 
     albums = facebook._client.get_list(me.id, 'albums')
 
-    count = 0
-    album_photos = 0
-    album_name = ''
+    album_photos = []
+    album_names = []
     for i in albums:
-        album_photos = facebook.get_photos(i.id)
-        album_name = i.name
-        for j in album_photos:
+        photos = facebook.get_photos(i.id)
+        album_names.append(i.name)
+
+        temp_phot = []
+        count = 0
+        for j in photos:
             if count < 10:
-                print j.picture
+                temp_phot.append(j.picture)
+                #print j.picture
             else:
                 break
-        break
+        album_photos.append(temp_phot)
     #print albums.
+    album_photos = zip(album_names,album_photos)
 
 
+    return render_template('photography.html', photos=photos, album_photos=album_photos, album_names=album_names)
 
-    return render_template('photography.html', photos=photos, album_photos=album_photos, album_name=album_name)
+
+@app.route('/resume')
+def resume():
+  return render_template('resume.html')
+
+
 
 #@app.route('/about')
 #def about():
